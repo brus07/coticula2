185debb1a3e102a0d8f21dbbd051a5939377ff18
brus07 <brus07@gmail.com>
2015-01-03 16:00:05 +0200

    Added default (empty) test.

---
+++ Coticula2.Test/DefaultTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coticula2.Test
+{
+    [TestFixture]
+    public class DefaultTests
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(5, Math.Max(3, 5));
+        }
+    }
+}
