0fb8b746442090c2c8945cbc70dae085ebc068ac
brus07 <brus07@gmail.com>
2015-02-08 17:17:04 +0200

    Created context and Configuration for migration.

---
+++ Coticula2Face/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Coticula2Face.Models
+{
+    public class Context : DbContext
+    {
+    }
+}
