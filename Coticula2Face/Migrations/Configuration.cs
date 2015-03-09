53f484e3a00d93dfef777422d724f2ccb5855f23
brus07 <brus07@gmail.com>
2015-03-09 11:53:41 +0200

    Updated for Migration and created initial migration.

--- Coticula2Face/Migrations/Configuration.cs
+++ Coticula2Face/Migrations/Configuration.cs
@@ -5,14 +5,15 @@ namespace Coticula2Face.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Coticula2Face.Models.Context>
+    internal sealed class Configuration : DbMigrationsConfiguration<Coticula2Face.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            ContextKey = "Coticula2Face.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(Coticula2Face.Models.Context context)
+        protected override void Seed(Coticula2Face.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
