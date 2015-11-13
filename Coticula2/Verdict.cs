24c9ba0377ba6e62ecec4f935ab9653070ce5197
Ruslan Babilya <ruslan.babilya@aspose.com>
2015-11-13 23:39:32 +0200

    Created simple logic for testing.

---
+++ Coticula2/Verdict.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coticula2
+{
+    public enum Verdict
+    {
+        Accepted,
+        CopilationError,
+        WrongAnswer,
+        RunTimeError,
+        TimeLimit
+    }
+}
