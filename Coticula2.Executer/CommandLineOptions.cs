af2540451c89f431eaddc4d56fb24bb200d0e1d1
Ruslan Babilya <ruslan.babilya@aspose.com>
2016-01-13 01:08:08 +0200

    Added simple executer for run testing process from console.

---
+++ Coticula2.Executer/CommandLineOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace Coticula2.Executer
+{
+    internal class CommandLineOptions
+    {
+        [Option('s', "solution", Required = true, HelpText = "The solution file.")]
+        public string SolutionFile { get; set; }
+
+        [Option('l', "language"/*, Required = true*/, HelpText = "The programming language id.")]
+        public int Language { get; set; }
+
+        [Option('t', "task", Required = true, HelpText = "The task id.")]
+        public int TaskId { get; set; }
+    }
+}
---
+++ Coticula2.Executer/Program.cs
@@ -0,0 +1,47 @@
+﻿using CommandLine;
+using Protex;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coticula2.Executer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CommandLineOptions commandLineOptions = null;
+
+            var arguments = Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(options => commandLineOptions = options);
+
+            if (arguments is NotParsed<CommandLineOptions>)
+            {
+                return;
+            }
+
+            if (arguments is Parsed<CommandLineOptions>)
+            {
+                IRunner runner = Protex.Windows.Creator.CreateRunner();
+                Tester tester = new Tester(runner);
+                Language language = Language.CSharp;
+                string solution = File.ReadAllText(commandLineOptions.SolutionFile);
+                var testingResult = tester.Test(commandLineOptions.TaskId, solution, language);
+
+
+                Console.WriteLine("Compilation Verdict: {0}", testingResult.CompilationVerdict);
+                Console.WriteLine("Compilation Output: {0}", testingResult.CompilationOutput);
+                if (testingResult.TestVerdicts != null)
+                    for (int i = 0; i < testingResult.TestVerdicts.Length; i++)
+                        Console.WriteLine("Verdict {0}: {1}", i, testingResult.TestVerdicts[i]);
+
+                //Console.WriteLine("Working time : {0}", result.WorkingTime);
+                //Console.WriteLine("Peak memory used: {0}", result.PeakMemoryUsed);
+                //Console.WriteLine("Exit code: {0}", result.ExitCode);
+            }
+        }
+    }
+}
