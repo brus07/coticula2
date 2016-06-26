f78eebdd05f4175f6ac223bc64560dbb929a4d26
brus07 <brus07@gmail.com>
2016-06-26 22:40:26 +0300

    Added test validator for validate input files.

---
+++ Coticula2.Test/Jobs/ValidatorProblemJobTest.cs
@@ -0,0 +1,102 @@
+﻿using Coticula2.Jobs;
+using Coticula2.Test.Mocks;
+using NUnit.Framework;
+
+namespace Coticula2.Test.Jobs
+{
+    [TestFixture]
+    public class ValidatorProblemJobTest
+    {
+        const string ValidatorForSwapProblem = @"using System;
+
+public class ValidatorForSwapProblem
+{
+    private static void Main()
+    {
+        string[] tokens = Console.ReadLine().Split();
+        if (tokens.Length != 2)
+        {
+                Console.WriteLine(""the number of parameters should be 2"");
+                return;
+        }
+        int a = int.Parse(tokens[0]);
+        int b = int.Parse(tokens[1]);
+        if (a < 0 || a > 1000)
+        {
+                Console.WriteLine(""a is bad"");
+                return;
+        }
+        if (b < 0 || b > 1000)
+        {
+                Console.WriteLine(""b is bad"");
+                return;
+        }
+        Console.WriteLine(""ok"");
+    }
+}";
+
+        [Test]
+        public void MainTestValidator()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 1, ValidatorForSwapProblem, Language.CSharp);
+            job.Execute();
+            var result = job.TestingResult;
+
+            Assert.AreEqual(Verdict.Accepted, result.CompilationVerdict);
+            Assert.Greater(result.TestVerdicts.Length, 0);
+            Assert.That(List.Map(result.TestVerdicts).Property("Verdict"), Is.All.EqualTo(Verdict.Accepted));
+        }
+
+        [Test]
+        public void MainTestValidatorFromProblem()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 1);
+            job.Execute();
+            var result = job.TestingResult;
+
+            Assert.AreEqual(Verdict.Accepted, result.CompilationVerdict);
+            Assert.Greater(result.TestVerdicts.Length, 0);
+            Assert.That(List.Map(result.TestVerdicts).Property("Verdict"), Is.All.EqualTo(Verdict.Accepted));
+        }
+
+        [Test]
+        public void IncorrectTestValidator()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 8801, ValidatorForSwapProblem, Language.CSharp);
+            job.Execute();
+            var result = job.TestingResult;
+
+            Assert.AreEqual(Verdict.Accepted, result.CompilationVerdict);
+            Assert.AreEqual(6, result.TestVerdicts.Length);
+            Assert.That(List.Map(result.TestVerdicts).Property("Verdict"), Is.All.EqualTo(Verdict.WrongAnswer));
+        }
+
+        [Test]
+        public void IncorrectTestValidatorFromProblem()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 8801);
+            job.Execute();
+            var result = job.TestingResult;
+
+            Assert.AreEqual(Verdict.Accepted, result.CompilationVerdict);
+            Assert.AreEqual(6, result.TestVerdicts.Length);
+            Assert.That(List.Map(result.TestVerdicts).Property("Verdict"), Is.All.EqualTo(Verdict.WrongAnswer));
+        }
+
+        [Test]
+        [ExpectedException]
+        public void IncorrectTestValidatorNoValidator()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 8811);
+            job.Execute();
+        }
+
+        [Test]
+        [ExpectedException]
+        public void IncorrectTestValidatorIncorrectLanguage()
+        {
+            ValidatorProblemJob job = new ValidatorProblemJob(new RunnerMock(), 8812);
+            job.Execute();
+        }
+    }
+}
---
+++ Coticula2/Jobs/ValidatorTestJob.cs
@@ -0,0 +1,84 @@
+﻿using Coticula2.Job;
+using Coticula2.Jobs.Comparers;
+using Protex;
+using Protex.Windows;
+using System;
+using System.IO;
+
+namespace Coticula2.Jobs
+{
+    public class ValidatorTestJob : IJob
+    {
+        private readonly IRunner Runner;
+        private readonly string ExecuteFilePath;
+        private readonly int ProblemId;
+        private readonly int TestId;
+
+        public ValidatorTestJob(IRunner runner, string executeFilePath, int problemId, int testId)
+        {
+            Runner = runner;
+            ExecuteFilePath = executeFilePath;
+            ProblemId = problemId;
+            TestId = testId;
+        }
+
+        public TestResult TestResult { get; private set; }
+
+        public void Execute()
+        {
+            string fullPathToProblem = FullPathToProblem(ProblemId);
+            var testDirectories = Directory.GetDirectories(fullPathToProblem, string.Format("test{0}", TestId));
+            if (testDirectories.Length != 1)
+                throw new InvalidOperationException();
+
+            var testDirectory = testDirectories[0];
+
+            var inputFiles = Directory.GetFiles(testDirectory, "in.txt");
+            if (inputFiles.Length != 1)
+            {
+                throw new FileNotFoundException(Path.Combine(testDirectory, "in.txt"));
+            }
+            string inputFile = inputFiles[0];
+
+            var testStartInfo = Creator.CreateRunnerStartInfo();
+            //HACK: need make universal command (for different languages)
+            testStartInfo.ExecutableFile = ExecuteFilePath;
+            testStartInfo.InputString = File.ReadAllText(inputFile);
+
+            var testExecutedResult = Runner.Run(testStartInfo);
+            Verdict currentVerdict = Verdict.Accepted;
+            if (testExecutedResult.ExitCode != 0)
+                currentVerdict = Verdict.RunTimeError;
+
+            //TODO: check for limits
+            if (testExecutedResult.ExitCode == -1)
+            {
+                if (testExecutedResult.WorkingTime >= testStartInfo.WorkingTimeLimit)
+                    currentVerdict = Verdict.TimeLimit;
+                if (testExecutedResult.PeakMemoryUsed >= testStartInfo.MemoryLimit)
+                    currentVerdict = Verdict.MemoryLimit;
+            }
+
+            //compare outputs
+            if (currentVerdict == Verdict.Accepted)
+            {
+                CompareOkOutputJob job = new CompareOkOutputJob(testExecutedResult.OutputString);
+                job.Execute();
+                if (!job.EqualOutputs)
+                    currentVerdict = Verdict.WrongAnswer;
+            }
+
+            TestResult = new TestResult() { TestId = TestId, Verdict = currentVerdict, WorkingTime = testExecutedResult.WorkingTime, PeakMemoryUsed = testExecutedResult.PeakMemoryUsed };
+            Console.WriteLine("Verdict: {0}; Time used: {1}ms; Memory used: {2}KiB;", currentVerdict, testExecutedResult.WorkingTime, testExecutedResult.PeakMemoryUsed);
+        }
+
+        public static string FullPathToProblem(int id)
+        {
+            string res = Path.Combine("Problems", "Problem" + id);
+            string toProblemsFolder = Path.Combine("..", "..", "..", "TestData");
+            res = Path.Combine(toProblemsFolder, res);
+            Directory.Exists(res);
+            return res;
+        }
+    }
+}
